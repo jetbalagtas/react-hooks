cf9aaaa559d83ddae14e29bc955fa8a7698eff8f
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-19 07:24:34 +0800

    the useEffect hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as db from '../endpoints';
 
@@ -6,13 +6,26 @@ const todo = props => {
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
 
+  useEffect(() => {
+    axios.get(db.POSTS_DB)
+    .then(result => {
+      console.log(result);
+      const todoData = result.data;
+      const todos = [];
+      for (const key in todoData) {
+        todos.push({id: key, name: todoData[key].name})
+      }
+      setTodoList(todos);
+    })
+  });
+
   const handleInputChange = e => {
     setTodoName(e.target.value);
   }
 
   const handleAddTodo = () => {
     setTodoList(todoList.concat(todoName));
-    axios.post(db.POST_DB, {name: todoName})
+    axios.post(db.POSTS_DB, {name: todoName})
     .then(res => {
       console.log(res);
     })
@@ -32,7 +45,7 @@ const todo = props => {
       <button type="button" onClick={handleAddTodo}>Add</button>
       <ul>
         {todoList.map(todo => (
-          <li key={todo}>{todo}</li>
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </React.Fragment>
