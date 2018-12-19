f30be1c3591732af6b42526ebfa5bd7e2d30a701
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-20 07:51:30 +0800

    convert App component and apply useState hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
-  }
+const app = props => {
+  const [page, setpage] = useState('auth');
+
+  const switchPage = pageName => {
+    setpage(pageName);
+  };
+
+  return (
+    <div className="App">
+      <Header
+        onLoadTodos={switchPage.bind(this, 'todos')}
+        onLoadAuth={switchPage.bind(this, 'auth')}
+      />
+      <hr />
+      {page === 'auth' ? <Auth /> : <Todo />}
+    </div>
+  );
 }
 
-export default App;
+export default app;
---
+++ src/components/Auth.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const auth = props => (
+  <h1>Auth Component</h1>
+);
+
+export default auth;
