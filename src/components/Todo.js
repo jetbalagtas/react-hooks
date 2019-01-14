d4a9875130814a044c5dab61f4672a89afd35712
Jet Balagtas <jetbalagtas@gmail.com>
2019-01-14 15:35:21 -0500

    using the useReducer() hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import * as db from '../endpoints';
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
   const [submittedTodo, setSubmittedTodo] = useState(null);
-  const [todoList, setTodoList] = useState([]);
+  // const [todoList, setTodoList] = useState([]);
+
+  const todoListReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD':
+        return state.concat(action.payload);
+      case 'SET':
+        return action.payload;
+      case 'REMOVE':
+        return state.filter(todo => todo.id !== action.payload);
+      default:
+        return state;
+    }
+  };
+
+  const [todoList, dispatch] = useReducer(todoListReducer, []);
 
   useEffect(() => {
     axios.get(db.POSTS_DB)
@@ -16,7 +31,7 @@ const todo = props => {
       for (const key in todoData) {
         todos.push({id: key, name: todoData[key].name})
       }
-      setTodoList(todos);
+      dispatch({type: 'SET', payload: todos});
     });
     return () => {
       console.log('Cleanup...');
@@ -36,7 +51,7 @@ const todo = props => {
 
   useEffect(() => {
     if (submittedTodo) {
-      setTodoList(todoList.concat(submittedTodo));
+      dispatch({type: 'ADD', payload: submittedTodo});
     }
   }, [submittedTodo]);
 
